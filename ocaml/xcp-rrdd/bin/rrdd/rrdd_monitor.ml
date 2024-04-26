a96c6a3c1e5f405858904742dec83dab84bf577b
Edwin Török <edwin.torok@cloud.com>
2024-04-26 15:14:27 +0100

    rrdd: avoid constructing intermediate lists, use Seq
    This enables extending the type without causing performance issues,
    and should reduce the work for the garbage collector.
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/xcp-rrdd/bin/rrdd/rrdd_monitor.ml
+++ ocaml/xcp-rrdd/bin/rrdd/rrdd_monitor.ml
@@ -107,7 +107,7 @@ let update_rrds timestamp dss uuid_domids paused_vms =
     in
     OwnerMap.update owner merge all
   in
-  let dss = List.fold_left consolidate OwnerMap.empty dss in
+  let dss = Seq.fold_left consolidate OwnerMap.empty dss in
 
   (* the first parameter and ds.ds_name are equivalent *)
   let to_named_updates (_, ds) =
@@ -133,9 +133,7 @@ let update_rrds timestamp dss uuid_domids paused_vms =
            unreliable"
           by_how_much ;
       let process_vm vm_uuid dss =
-        let named_updates =
-          StringMap.to_seq dss |> Seq.map to_named_updates |> List.of_seq
-        in
+        let named_updates = StringMap.to_seq dss |> Seq.map to_named_updates in
         let dss = StringMap.to_seq dss |> Seq.map snd |> List.of_seq in
 
         match StringMap.find_opt vm_uuid uuid_domids with
@@ -165,9 +163,7 @@ let update_rrds timestamp dss uuid_domids paused_vms =
               __FUNCTION__ vm_uuid
       in
       let process_sr sr_uuid dss =
-        let named_updates =
-          StringMap.to_seq dss |> Seq.map to_named_updates |> List.of_seq
-        in
+        let named_updates = StringMap.to_seq dss |> Seq.map to_named_updates in
         let dss = StringMap.to_seq dss |> Seq.map snd |> List.of_seq in
         try
           (* First, potentially update the rrd with any new default dss *)
@@ -185,9 +181,7 @@ let update_rrds timestamp dss uuid_domids paused_vms =
         with _ -> log_backtrace ()
       in
       let process_host dss =
-        let named_updates =
-          StringMap.to_seq dss |> Seq.map to_named_updates |> List.of_seq
-        in
+        let named_updates = StringMap.to_seq dss |> Seq.map to_named_updates in
         let dss = StringMap.to_seq dss |> Seq.map snd |> List.of_seq in
 
         match !host_rrd with
