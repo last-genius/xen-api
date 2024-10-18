092d26001207b52664f2302c25f52191e04df7a0
Andrii Sultanov <andrii.sultanov@cloud.com>
2024-10-18 13:18:48 +0100

    CA-391651: Update RRD tests to the new interfaces
    Signed-off-by: Andrii Sultanov <andrii.sultanov@cloud.com>

--- ocaml/libs/xapi-rrd/lib_test/unit_tests.ml
+++ ocaml/libs/xapi-rrd/lib_test/unit_tests.ml
@@ -15,10 +15,7 @@ let assert_ds_equal d1 d2 =
   compare_float __LOC__ d1.ds_value d2.ds_value ;
   compare_float __LOC__ d1.ds_unknown_sec d2.ds_unknown_sec
 
-let assert_dss_equal d1s d2s =
-  let d1s = Array.to_list d1s in
-  let d2s = Array.to_list d2s in
-  List.iter2 assert_ds_equal d1s d2s
+let assert_dss_equal d1s d2s = Array.iter2 assert_ds_equal d1s d2s
 
 let assert_cdp_prep_equal c1 c2 =
   compare_float __LOC__ c1.cdp_value c2.cdp_value ;
@@ -37,18 +34,15 @@ let assert_rra_equal a1 a2 =
   Alcotest.(check int) __LOC__ a1.rra_row_cnt a2.rra_row_cnt ;
   Alcotest.(check int) __LOC__ a1.rra_pdp_cnt a2.rra_pdp_cnt ;
   compare_float __LOC__ a1.rra_xff a2.rra_xff ;
-  List.iter2 assert_cdp_prep_equal
-    (Array.to_list a1.rra_cdps)
-    (Array.to_list a2.rra_cdps) ;
-  List.iter2 assert_fring_equal
-    (Array.to_list a1.rra_data)
-    (Array.to_list a2.rra_data)
+  Array.iter2 assert_cdp_prep_equal a1.rra_cdps a2.rra_cdps ;
+  Array.iter2 assert_fring_equal a1.rra_data a2.rra_data
 
-let assert_rras_equal a1s a2s =
-  List.iter2 assert_rra_equal (Array.to_list a1s) (Array.to_list a2s)
+let assert_rras_equal a1s a2s = Array.iter2 assert_rra_equal a1s a2s
 
 let assert_rrds_equal r1 r2 =
-  compare_float __LOC__ r1.last_updated r2.last_updated ;
+  compare_float __LOC__
+    (Rrd.most_recently_last_updated r1.last_updated)
+    (Rrd.most_recently_last_updated r2.last_updated) ;
   Alcotest.(check int64) __LOC__ r1.timestep r2.timestep ;
   assert_dss_equal r1.rrd_dss r2.rrd_dss ;
   assert_rras_equal r1.rrd_rras r2.rrd_rras
@@ -74,9 +68,7 @@ let test_ranges rrd () =
   let in_range_fring ds fring =
     in_range ds.ds_min ds.ds_max (fring_to_list fring)
   in
-  let in_range_rra dss rra =
-    List.iter2 in_range_fring dss (Array.to_list rra.rra_data)
-  in
+  let in_range_rra dss rra = Array.iter2 in_range_fring dss rra.rra_data in
   let range_is_not_empty ds =
     Alcotest.(check bool)
       (Printf.sprintf "min (%f) < max (%f); " ds.ds_min ds.ds_max)
@@ -84,9 +76,7 @@ let test_ranges rrd () =
   in
 
   Array.iter range_is_not_empty rrd.rrd_dss ;
-  List.iter
-    (in_range_rra @@ Array.to_list rrd.rrd_dss)
-    (Array.to_list rrd.rrd_rras)
+  Array.iter (in_range_rra @@ rrd.rrd_dss) rrd.rrd_rras
 
 let test_marshall rrd ~json () =
   ignore
@@ -124,21 +114,31 @@ let gauge_rrd =
   let rra2 = rra_create CF_Average 100 10 0.5 in
   let rra3 = rra_create CF_Average 100 100 0.5 in
   let rra4 = rra_create CF_Average 100 1000 0.5 in
+  let ts = 1000000000.0 in
   let ds = ds_create "foo" Gauge ~mrhb:10.0 (VT_Float 0.0) in
   let ds2 = ds_create "bar" Gauge ~mrhb:10.0 (VT_Float 0.0) in
   let ds3 = ds_create "baz" Gauge ~mrhb:10.0 (VT_Float 0.0) in
   let ds4 = ds_create "boo" Gauge ~mrhb:10.0 (VT_Float 0.0) in
   let rrd =
-    rrd_create [|ds; ds2; ds3; ds4|] [|rra; rra2; rra3; rra4|] 1L 1000000000.0
+    rrd_create "gauge_test" [|ds; ds2; ds3; ds4|] [|rra; rra2; rra3; rra4|] 1L
+      ts
   in
   let id = Identity in
   for i = 1 to 100000 do
     let t = 1000000000.0 +. (0.7 *. float_of_int i) in
-    let v1 = VT_Float (0.5 +. (0.5 *. sin (t /. 10.0))) in
-    let v2 = VT_Float (1.5 +. (0.5 *. cos (t /. 80.0))) in
-    let v3 = VT_Float (3.5 +. (0.5 *. sin (t /. 700.0))) in
-    let v4 = VT_Float (6.5 +. (0.5 *. cos (t /. 5000.0))) in
-    ds_update rrd t [|v1; v2; v3; v4|] [|id; id; id; id|] false
+    let v1 =
+      (0, {value= VT_Float (0.5 +. (0.5 *. sin (t /. 10.0))); transform= id})
+    in
+    let v2 =
+      (1, {value= VT_Float (1.5 +. (0.5 *. cos (t /. 80.0))); transform= id})
+    in
+    let v3 =
+      (2, {value= VT_Float (3.5 +. (0.5 *. sin (t /. 700.0))); transform= id})
+    in
+    let v4 =
+      (3, {value= VT_Float (6.5 +. (0.5 *. cos (t /. 5000.0))); transform= id})
+    in
+    ds_update rrd "gauge_test" t [|v1; v2; v3; v4|] false
   done ;
   rrd
 
@@ -150,66 +150,60 @@ let of_file filename =
 (* Used to generate flip_flop.xml for test_ca_325844, then gets edited manually
    to set min to 0 *)
 let _deserialize_verify_rrd =
-  let init_time = 0. in
-
   let rra1 = rra_create CF_Average 100 1 0.5 in
   let rra2 = rra_create CF_Min 100 1 0.5 in
   let rra3 = rra_create CF_Max 100 1 0.5 in
   let ds = ds_create "flip_flop" Derive (VT_Int64 0L) in
 
-  let rrd = rrd_create [|ds|] [|rra1; rra2; rra3|] 5L init_time in
+  let rrd = rrd_create "deserialize_verify" [|ds|] [|rra1; rra2; rra3|] 5L 0. in
 
   let id = Identity in
   for i = 1 to 100 do
-    let t = init_time +. float_of_int i in
+    let t = float_of_int i in
     let t64 = Int64.of_float t in
-    let v = VT_Int64 Int64.(mul t64 (mul (-1L) (rem t64 2L))) in
-    ds_update rrd t [|v|] [|id|] false
+    let value = VT_Int64 Int64.(mul t64 (mul (-1L) (rem t64 2L))) in
+    ds_update rrd "deserialize_verify" t [|(0, {value; transform= id})|] false
   done ;
   rrd
 
 let ca_322008_rrd =
-  let init_time = 0. in
-
   let rra1 = rra_create CF_Average 100 1 0.5 in
   let rra2 = rra_create CF_Min 100 1 0.5 in
   let rra3 = rra_create CF_Max 100 1 0.5 in
   let ds = ds_create "even or zero" Derive ~min:0. (VT_Int64 0L) in
 
-  let rrd = rrd_create [|ds|] [|rra1; rra2; rra3|] 5L init_time in
+  let rrd = rrd_create "ca_322008_rrd_test" [|ds|] [|rra1; rra2; rra3|] 5L 0. in
 
   let id = Identity in
 
   for i = 1 to 100000 do
-    let t = init_time +. float_of_int i in
+    let t = float_of_int i in
     let t64 = Int64.of_float t in
-    let v = VT_Int64 (Int64.mul t64 (Int64.rem t64 2L)) in
-    ds_update rrd t [|v|] [|id|] false
+    let value = VT_Int64 (Int64.mul t64 (Int64.rem t64 2L)) in
+    ds_update rrd "ca_322008_rrd_test" t [|(0, {value; transform= id})|] false
   done ;
   rrd
 
 let ca_329043_rrd_1 =
-  let init_time = 0. in
-
   let rra1 = rra_create CF_Average 3 1 0.5 in
   let rra2 = rra_create CF_Min 3 1 0.5 in
   let rra3 = rra_create CF_Max 3 1 0.5 in
   let ds = ds_create "derive_with_min" ~min:0. ~max:1. Derive VT_Unknown in
 
-  let rrd = rrd_create [|ds|] [|rra1; rra2; rra3|] 5L init_time in
+  let rrd = rrd_create "ca_329043_rrd_test" [|ds|] [|rra1; rra2; rra3|] 5L 0. in
 
   let id = Identity in
 
   let time_value_of_i i =
-    let t = 5. *. (init_time +. float_of_int i) in
+    let t = 5. *. float_of_int i in
     if i = 1 then
       (t, VT_Int64 0L)
     else
       (t, VT_Int64 Int64.(of_float t))
   in
   for i = 0 to 4 do
-    let t, v = time_value_of_i i in
-    ds_update rrd t [|v|] [|id|] (i = 0)
+    let t, value = time_value_of_i i in
+    ds_update rrd "ca_329043_rrd_test" t [|(0, {value; transform= id})|] (i = 0)
   done ;
   rrd
 
@@ -225,7 +219,8 @@ let create_rrd ?(rows = 2) values min max =
   let ds3 = ds_create "gauge" ~min ~max Derive VT_Unknown in
 
   let rrd =
-    rrd_create [|ds1; ds2; ds3|] [|rra1; rra2; rra3; rra4|] 5L init_time
+    rrd_create "create_rrd_test" [|ds1; ds2; ds3|] [|rra1; rra2; rra3; rra4|] 5L
+      init_time
   in
 
   let id = Identity in
@@ -233,7 +228,9 @@ let create_rrd ?(rows = 2) values min max =
   List.iteri
     (fun i v ->
       let t = 5. *. (init_time +. float_of_int i) in
-      ds_update rrd t [|VT_Int64 v|] [|id; id; id; id|] (i = 0)
+      ds_update rrd "create_rrd_test" t
+        [|(0, {value= VT_Int64 v; transform= id})|]
+        (i = 0)
     )
     values ;
   rrd
@@ -247,7 +244,7 @@ let ca_329043_rrd_2 =
 let ca_329813_rrd =
   let rrd = create_rrd [0L; 5L; 10L] 0. 1. in
   let new_ds = ds_create "new!" Derive VT_Unknown in
-  Rrd.rrd_add_ds rrd rrd.last_updated new_ds
+  Rrd.rrd_add_ds rrd (Rrd.most_recently_last_updated rrd.last_updated) new_ds
 
 let test_ca_322008 () =
   let rrd = ca_322008_rrd in
@@ -256,13 +253,12 @@ let test_ca_322008 () =
    * the time in seconds when it was last updated, setting max
    * value may cause the bug to not trigger *)
   let in_range_fring ds fring =
-    in_range ds.ds_min rrd.last_updated (fring_to_list fring)
+    in_range ds.ds_min
+      (Rrd.most_recently_last_updated rrd.last_updated)
+      (fring_to_list fring)
   in
-  let in_range_rra dss rra =
-    List.iter2 in_range_fring dss (Array.to_list rra.rra_data)
-  in
-  List.iter (in_range_rra @@ Array.to_list rrd.rrd_dss)
-  @@ Array.to_list rrd.rrd_rras
+  let in_range_rra dss rra = Array.iter2 in_range_fring dss rra.rra_data in
+  Array.iter (in_range_rra @@ rrd.rrd_dss) @@ rrd.rrd_rras
 
 let test_ca_325844 () =
   let rrd = of_file (Filename.concat "test_data" "flip_flop.xml") in
