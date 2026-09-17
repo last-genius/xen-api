b6508043da2dfe413d2c4e5595310127be1a7221
Andrii Sultanov <andriy.sultanov@vates.tech>
2026-09-17 07:53:27 +0000

    libs/vhd: Add auxiliary information to every `Sector element
    Since expand_copy only forwards a chunk of the originally allocated buffer (it
    cuts off the original with Cstruct.cut), when the caller function completes, it
    can't recover the originally allocated buffer to re-use it.
    
    To allow the buffers to be served from a pool of pre-allocated Cstruct.t, carry
    an additional integer with every `Sector as metadata helping to identify the
    original buffer.
    
    No users currently care about this additional metadata, so set it to -1 and
    ignore in all the current users - no functional change.
    
    Signed-off-by: Andrii Sultanov <andriy.sultanov@vates.tech>

--- ocaml/libs/vhd/vhd_format/element.ml
+++ ocaml/libs/vhd/vhd_format/element.ml
@@ -13,7 +13,7 @@
  *)
 
 type 'a t =
-  [`Copy of 'a * int64 * int64 | `Sectors of Cstruct.t | `Empty of int64]
+  [`Copy of 'a * int64 * int64 | `Sectors of Cstruct.t * int | `Empty of int64]
 
 let sector_size = 512
 
@@ -22,7 +22,7 @@ let to_string = function
       Printf.sprintf "1 sector copied starting at offset %Ld" offset
   | `Copy (_, offset, len) ->
       Printf.sprintf "%Ld sectors copied starting at offset %Ld" len offset
-  | `Sectors x ->
+  | `Sectors (x, _) ->
       let text = String.escaped (Cstruct.to_string (Cstruct.sub x 0 16)) in
       if Cstruct.length x = sector_size then
         Printf.sprintf "1 sector \"%s...\"" text
@@ -38,7 +38,7 @@ let to_string = function
 let len = function
   | `Copy (_, _, len) ->
       len
-  | `Sectors x ->
+  | `Sectors (x, _) ->
       Int64.of_int (Cstruct.length x / sector_size)
   | `Empty x ->
       x
